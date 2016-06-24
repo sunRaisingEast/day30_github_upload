5523bba32acbcea8b2ad7ec5d9f088215cccf552
sunRaisingEast <245394175@qq.com>
2016-06-24 16:38:31 +0800

    Initial commit

---
+++ app/src/main/java/com/dream/huangchen/day30_github_upload/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dream.huangchen.day30_github_upload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
